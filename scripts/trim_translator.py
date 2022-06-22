3a38e4e5f6bc2a39514cb5977e3126a61baa460f
AlexShen101 <alexander.shen@gmail.com>
2022-06-22 17:15:33 -0400

    06-22-2022 Changes

--- scripts/trim_translator.py
+++ scripts/trim_translator.py
@@ -1,6 +1,24 @@
 import json
 
-filePath = './test_outputs/translator_en_us.json'
-newFilePath = './test_outputs/new_translator_en_us.json'
+filePath = './scripts/test_outputs/translator_en_us.json'
+newFilePath = './scripts/test_outputs/new_translator_en_us.json'
 
-with open(filePath, 'a') as file:
+with open(filePath, 'r') as file:
+    data = json.load(file)
+    
+    for key in dict(data):
+        if "TEXT_DIALOGUE" in key:
+            del data[key]
+        elif "TEXT_ACHV" in key:
+            del data[key]
+        elif "TEXT_FISH" in key:
+            del data[key]
+        elif "TEXT_FOOLSDAY" in key:
+            del data[key]
+
+    with open(newFilePath, 'w') as newFile:
+        newFile.write(json.dumps(data, indent=4))
+    
+    
+
+
